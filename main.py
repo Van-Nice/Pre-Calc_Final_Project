67cf0bab0557ddb952fa540e2690f34b5040abd3
Van-Nice <wilsonwalter808@gmail.com>
2023-04-18 23:44:41 -0600

    Final touches

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# Wilson Van Nice
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math
 
-# initialize aries
 minutes = [7, 15, 23, 31, 38, 45, 53, 60, 6, 13, 19, 24, 30, 35, 39, 43]
 seconds = [51, 40, 25, 4, 34, 56, 5, 0, 40, 3, 7, 51, 13, 12, 46, 55]
 degree = 0
@@ -17,7 +18,6 @@ def decimal_calc(minute: float, second: float):
 
 # fill empty lists with corresponding values
 for i in range(0, 16):
-    # aries calculations
     degree += 3.75
     theta.append(degree)
     vara_decimals.append(decimal_calc(minutes[i], seconds[i]))
@@ -32,4 +32,3 @@ graph = sns.pointplot(x='Theta', y='value', hue='variable', data=pd.melt(data_fr
 graph.set(ylabel="Numeric Value", title='Sine vs. sin')
 plt.xticks(rotation = 45)
 plt.show()
-
